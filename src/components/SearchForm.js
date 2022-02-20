e390224637b550cf413d70bcd4c1ab86e4aebf6b
Cleyra <cleyra.uzcategui@gmail.com>
2022-02-20 19:51:34 +0000

    Added logic to conect search box with search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
-const SearchForm = () => {
+const SearchForm = ({onSearchSubmit}) => {
+
+const [text, setText] = useState("");
+
+const handleTextChange = (evt) => {
+    setText(evt.target.value);
+  }
+
+  const handleSearchSubmit = (evt) => {
+    evt.preventDefault();
+    const textToSubmit = text.trim();
+    if (!textToSubmit){
+      return
+    }
+
+   onSearchSubmit({
+      searchTerm: textToSubmit
+    });
+
+    setText("");
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSearchSubmit}>
       <input 
         type="text"
         placeholder="Search something..."
+        value={text}
+        onChange={handleTextChange}
       />
       <button 
         type="submit"
--- src/containers/SearchBox.js
+++ src/containers/SearchBox.js
@@ -4,41 +4,49 @@ import SearchForm from "../components/SearchForm";
 
 const SearchBox = () => {
 
+    const mockData = [
+        {
+           "title": "Tempor Iisque",
+           "description": "sapien adversarium possim",
+           "tags": [
+             "delenit",
+             "eripuit"
+           ],
+           "weight": 2
+         },
+         {
+           "title": "Turpis ad",
+           "description": "expetenda tempor massa taciti falli viverra",
+           "tags": [
+             "signiferumque",
+             "mentitum"
+           ],
+           "weight": 3
+         },
+         {
+           "title": "Urbanitas Ubique Pertinacia Disputationi Disputationi",
+           "description": "lobortis maximus propriae falli elementum qualisque convenire ultrices aliquet non constituam periculis erroribus quisque",
+           "tags": [
+             "maiestatis",
+             "alterum"
+           ],
+           "weight": 10
+         }
+       ]
+
   const [results, setSearchs] = useState(
-    [
-       {
-          "title": "Tempor Iisque",
-          "description": "sapien adversarium possim",
-          "tags": [
-            "delenit",
-            "eripuit"
-          ],
-          "weight": 2
-        },
-        {
-          "title": "Turpis ad",
-          "description": "expetenda tempor massa taciti falli viverra",
-          "tags": [
-            "signiferumque",
-            "mentitum"
-          ],
-          "weight": 3
-        },
-        {
-          "title": "Urbanitas Ubique Pertinacia Disputationi Disputationi",
-          "description": "lobortis maximus propriae falli elementum qualisque convenire ultrices aliquet non constituam periculis erroribus quisque",
-          "tags": [
-            "maiestatis",
-            "alterum"
-          ],
-          "weight": 10
-        }
-      ]
+    []
   )
+
+  const sendRequest = (searchRequest) => {
+    setSearchs(mockData)
+    console.log(searchRequest)
+}
+
 
   return (
     <>
-        <SearchForm />
+        <SearchForm onSearchSubmit={(searchRequest) => sendRequest(searchRequest)} />
         <ResultList results={results} />
     </>
   );
